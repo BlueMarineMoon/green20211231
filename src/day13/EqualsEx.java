9a48e5aa3ab208fca459f3939ab729dec0388ad0
BlueMarineMoon <minibus2004@naver.com>
2021-12-31 16:10:38 +0900

    6교시 수업 후 백업

--- src/day13/EqualsEx.java
+++ src/day13/EqualsEx.java
@@ -1,8 +1,8 @@
 package day13;
 
-class Point {
+class Point1 {
 	int x, y;
-	public Point(int x, int y) {
+	public Point1(int x, int y) {
 		this.x = x; this.y = y;
 	}
 	public boolean equals(Object obj) {
@@ -14,11 +14,11 @@ class Point {
 
 public class EqualsEx {
 	public static void main(String[] args) {
-		Point a = new Point(2,3);
-		Point b = new Point(2,3);
-		Point c = new Point(3,4);
+		Point1 a = new Point1(2,3);
+		Point1 b = new Point1(2,3);
+		Point1 c = new Point1(3,4);
 		
-		if(a == b) // false
+		if(a == b) // false  <-- 인스턴스의 참조값 비교
 			System.out.println("a == b");
 		if(a.equals(b)) // true
 			System.out.println("a is equal to b");
